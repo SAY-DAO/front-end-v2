98bc0b6bf2f8938b3ffb56136478294d5a9b380c
ehsan-g <ehsan@say.company>
2021-05-25 11:11:52 -0400

    cleanup

--- src/components/CustomButton.js
+++ src/components/CustomButton.js
@@ -1,7 +1,7 @@
 import { Button } from "@material-ui/core";
 import { PropTypes } from "prop-types";
 import React from "react";
-import { withStyles } from "@material-ui/styles";
+import { withStyles } from "@material-ui/core/styles";
 import { withTranslation } from "react-i18next";
 
 const styles = {
