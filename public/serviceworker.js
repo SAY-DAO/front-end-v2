18bdba9620a7fcc63ea02e16e1fe826badfb792a
ehsan-g <ehsan@say.company>
2021-07-31 23:04:51 -0400

    refactored service worker

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,4 +1,4 @@
-// Guideline: https://web.dev/offline-cookbook/
+/* eslint-disable no-undef */
 const staticCacheName = 'SAY-v0.0.0';
 const urlsToCache = [
   '/static/js/main.chunk.js',
@@ -16,19 +16,51 @@ const urlsToCache = [
   '/index.html',
   '/manifest.json',
   '/',
-  // '/offline.html',
+  '/offline.html',
 ];
 
 const self = this;
 
 // install Service Worker and save cache to Application - Cache
 self.addEventListener('install', (event) => {
-  console.log('Attempting to install service worker and cache static assets');
+  // console.log('Attempting to install service worker and cache static assets');
   event.waitUntil(
-    caches.open(staticCacheName).then((cache) => cache.addAll(urlsToCache))
+    caches.open(staticCacheName).then((cache) =>
+      // console.log('Opened cache');
+      cache.addAll(urlsToCache)
+    )
   );
 });
+
 // Serve files from the cache
+self.addEventListener('fetch', (event) => {
+  // console.log('Fetch event for ', event.request.url);
+  event.respondWith(
+    caches
+      .match(event.request)
+      .then((response) => {
+        if (response) {
+          console.log('Found ', event.request.url, ' in cache');
+          return response;
+        }
+        // If fails, we send the request to the network.
+        console.log('Network request for ', event.request.url);
+        return fetch(event.request).then((res) =>
+          // TODO 5 - Respond with custom 404 page
+          caches.open(staticCacheName).then((cache) => {
+            cache.put(event.request.url, res.clone());
+            return res;
+          })
+        );
+      })
+      .catch((error) => {
+        // Respond with custom offline page
+        console.log('offline error ', error);
+        // return caches.match('offline.html'); // will ineterfere with action error responses
+      })
+  );
+});
+
 self.addEventListener('fetch', (event) => {
   console.log('Fetch event for ', event.request.url);
   event.respondWith(
@@ -44,28 +76,27 @@ self.addEventListener('fetch', (event) => {
 
         // TODO 4 - Add fetched files to the cache
       })
-      .then((response) => {
-        // TODO 5 - Respond with custom 404 page
-        caches.open(staticCacheName).then((cache) => {
-          cache.put(event.request.url, response.clone());
-          return response;
-        });
-      })
       .catch((error) => {
         // TODO 6 - Respond with custom offline page
-        console.log('offline error ', error);
       })
   );
 });
 
 // Delete outdated caches
 self.addEventListener('activate', (event) => {
-  console.log('Activating new service worker...');
+  // console.log('Activating new service worker...');
+
+  const cacheAllowlist = [staticCacheName];
+
   event.waitUntil(
-    caches
-      .keys()
-      .then((cacheNames) =>
-        Promise.all(cacheNames.map((cacheName) => caches.delete(cacheName)))
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((cacheName) => {
+          if (cacheAllowlist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
       )
+    )
   );
 });
