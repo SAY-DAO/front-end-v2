2084051a7592bc4cdb0139c4ff2391d7b0939213
Ehsan G <ehsan@say.company>
2022-02-05 20:09:57 -0500

    the landing page

--- src/components/main/DAO.jsx
+++ src/components/main/DAO.jsx
@@ -1,17 +1,13 @@
-/* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-no-bind */
 /* eslint-disable react-hooks/exhaustive-deps */
 import React, { useEffect, useState } from 'react';
-import { Grid, Divider, Typography, Avatar, Button } from '@mui/material';
+import { Grid, Alert, Link } from '@mui/material';
 import { useTranslation } from 'react-i18next';
 import { useHistory } from 'react-router';
 import { useDispatch, useSelector } from 'react-redux';
-import i18next from 'i18next';
 import CircularProgress from '@material-ui/core/CircularProgress';
-import { ResponsivePie } from '@nivo/pie';
 import PropTypes from 'prop-types';
 import { ResponsiveNetwork } from '@nivo/network';
-import { fetchMyHome } from '../../actions/main/homeAction';
 import { fetchUserDetails } from '../../actions/userAction';
 import AppBarBottom from './AppBarBottom';
 import { fetchFamilyNetworks } from '../../actions/DaoAction';
@@ -21,8 +17,8 @@ const DAO = () => {
   const history = useHistory();
 
   const [data, setData] = useState();
-  const [childNumber, setChildNumber] = useState(0);
-  const [childFamily, setChildFamily] = useState();
+  const [clicked, setClicked] = useState(false);
+  const [loading, setLoading] = useState(false);
 
   const userLogin = useSelector((state) => state.userLogin);
   const { userInfo, success: successLogin } = userLogin;
@@ -35,7 +31,9 @@ const DAO = () => {
 
   // login
   useEffect(() => {
-    dispatch(fetchFamilyNetworks());
+    if (!network) {
+      dispatch(fetchFamilyNetworks());
+    }
     dispatch(fetchUserDetails());
     if (errorUserDetails) {
       history.push('/login?redirect=main/home');
@@ -43,47 +41,52 @@ const DAO = () => {
   }, [userInfo, successLogin, history, errorUserDetails]);
 
   // add child node every ... second
-  useEffect(() => {
-    if (network && childNumber <= network.length) {
-      console.log('interval');
-      let now = childNumber;
-      const t = setInterval(() => {
-        now += 1;
-        setChildNumber(now);
-        if (now > network.length) {
-          clearInterval(t);
-        }
-      }, 200);
-    }
-  }, []);
+  // useEffect(() => {
+  //   if (network && childNumber <= network.length) {
+  //     console.log('interval');
+  //     let now = childNumber;
+  //     // const t = setInterval(() => {
+  //     now += 1;
+  //     setChildNumber(now);
+  //     // if (now > network.length) {
+  //     //   clearInterval(t);
+  //     // }
+  //     // }, 200);
+  //   }
+  // }, []);
 
   // custom node
   useEffect(() => {
-    if (network && network[0] && childNumber <= network.length) {
+    setClicked(false);
+    console.log('Children Nodes');
+    if (network && network[0] && !data) {
       let link = {};
       let nodesList = {};
       // 1 -start with only SAY in center
       nodesList = {
         nodes: [
+          // SAY in center
           {
             id: 'Node 0',
             height: 1,
             size: 40,
-            color: 'rgb(97, 25, 87)',
+            color: '#f8d5af',
             img: '/images/logo.png',
-            theId: 1,
+            theId: 0,
           },
         ],
+        // empty list
         links: [],
       };
+
       // 2- for every child create a node
-      for (let c = 0; c < childNumber; c += 1) {
+      for (let c = 0; c < network.length; c += 1) {
         const childNode = {
           id: `Node ${c + 1}`,
           height: 1,
-          size: 20,
+          size: 25,
           color: 'rgb(97, 205, 187)',
-          img: network[c].avatarUrl || '/images/logo.png',
+          img: network[c].avatarUrl,
           theId: network[c].id,
         };
         nodesList.nodes.push(childNode);
@@ -98,21 +101,23 @@ const DAO = () => {
       }
       setData(nodesList);
     }
-  }, [network, childNumber, data]);
+  }, [network, clicked]);
 
   // handle circle click
   function handleClick(e, node) {
+    setClicked(true);
+    console.log(data);
     let counter = 0;
     const nodesList = data;
     let prevId = '';
     let prevCircle = document.getElementById(prevId);
 
     if (!node.id.includes('.')) {
+      setLoading(true);
       // select element and increase 'r'
       const activeId = e.target.id;
-      const thenum = Number(node.id.replace(/^\D+/g, '')); // thenum = 37
-      console.log(node);
-      console.log(thenum);
+      const theId = Number(node.id.replace(/^\D+/g, '')); // theId = 37
+
       if (prevId !== '') {
         prevCircle.setAttribute('r', 15);
       }
@@ -131,7 +136,6 @@ const DAO = () => {
           }
         }
       }
-
       for (let f = 0; f < nodesList.nodes.length; f += 1) {
         if (nodesList.nodes[f].id.includes('.')) {
           if (f > -1) {
@@ -141,33 +145,35 @@ const DAO = () => {
       }
       setData(nodesList);
 
-      // 4- create nodes of child's V-family
+      // create new links
       for (
         let f = 0;
-        f < network[thenum - 1].family.currentMembers.length;
+        f < network[theId - 1].family.currentMembers.length;
         f += 1
       ) {
         counter += 1;
+        // 4- create V-family nodes
         const familyNode = {
           id: `Node ${counter}.0`,
           height: 1,
           size: 4,
           color: 'rgb(232, 193, 160)',
           img:
-            network[thenum - 1].family.currentMembers[f].avatarUrl ||
+            network[theId - 1].family.currentMembers[f].avatarUrl ||
             '/images/logo.png',
-          theId: (thenum + f) * counter, // TODO: use user's id
+          theId: (theId + f) * counter, // TODO: use user's id
         };
         // 5- create family and child link
         const link = {
-          source: `Node ${thenum}`,
+          source: `Node ${theId}`,
           target: `Node ${counter}.0`,
           distance: 15,
         };
         nodesList.links.push(link);
         nodesList.nodes.push(familyNode);
+        setData(nodesList);
       }
-      setChildFamily(thenum);
+      // 6 -click on the users
     } else {
       // select element and increase 'r'
       const activeId = e.target.id;
@@ -180,44 +186,48 @@ const DAO = () => {
       circle.setAttribute('r', 30);
       prevId = e.target.id;
     }
-
-    console.log(nodesList);
-    setData(nodesList);
+    console.log(data);
   }
 
   // custom node
-  const CustomPoint = ({ node }) => (
-    <svg id="graph" width="100%" height="800px">
-      <pattern
-        id={`nodes.${node.data.theId}`}
-        x="0%"
-        y="0%"
-        height="100%"
-        width="100%"
-        viewBox="0 0 512 512"
-      >
-        <image
+  const CustomPoint = ({ node }) => {
+    CustomPoint.propTypes = {
+      node: PropTypes.object,
+    };
+
+    return (
+      <svg id="graph" width="100%" height="800px">
+        <pattern
+          id={`nodes.${node.data.theId}`}
           x="0%"
           y="0%"
-          width="512"
-          height="512"
-          xlinkHref={node.data.img}
-        />
-      </pattern>
+          height="100%"
+          width="100%"
+          viewBox="0 0 512 512"
+        >
+          <image
+            x="0%"
+            y="0%"
+            width="512"
+            height="512"
+            xlinkHref={node.data.img}
+          />
+        </pattern>
 
-      <circle
-        id={`node.${node.id}`}
-        r={node.size}
-        opacity="1"
-        transform={`translate(${node.x}, ${node.y}) scale(1)`}
-        onClick={(e) => handleClick(e, node)}
-        className="medium"
-        fill={`url(#nodes.${node.data.theId})`}
-        stroke="lightblue"
-        strokeWidth="0.1%"
-      />
-    </svg>
-  );
+        <circle
+          id={`node.${node.id}`}
+          r={node.size}
+          opacity="1"
+          transform={`translate(${node.x}, ${node.y}) scale(1)`}
+          onClick={(e) => handleClick(e, node)}
+          className="medium"
+          fill={`url(#nodes.${node.data.theId})`}
+          stroke="lightblue"
+          strokeWidth="0.1%"
+        />
+      </svg>
+    );
+  };
 
   return (
     <Grid
@@ -228,16 +238,14 @@ const DAO = () => {
       sx={{
         paddingLeft: 2,
         paddingRight: 2,
-        minHeight: '100vh',
         overflow: 'scroll',
       }}
     >
       {!loadingNetwork && successHome && network && data ? (
         <div
           style={{
-            height: '100vh',
-            width: '200%',
-            direction: 'ltr',
+            height: '70vh',
+            width: '100%',
           }}
         >
           <ResponsiveNetwork
@@ -271,6 +279,12 @@ const DAO = () => {
       ) : (
         <CircularProgress />
       )}
+      <Alert severity="info">
+        To stay updated with our road map or contribute in building the DAO,
+        please visit our document page —
+        <Link href="https://road-map-docs.pages.dev/"> check it out! </Link>
+      </Alert>
+      <Grid sx={{ width: '100%' }} />
       <AppBarBottom path="dao" />
     </Grid>
   );
