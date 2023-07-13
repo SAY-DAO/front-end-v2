21ca2913e4c44d8402c07b7f6a0ee22cf794da5a
ehsan <ehsan@say.company>
2023-07-13 03:35:52 -0400

    bug fixes

--- src/pages/auth/Login.jsx
+++ src/pages/auth/Login.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 /* eslint-disable no-nested-ternary */
 import React, { useState, useEffect } from 'react';
 import { useTranslation, Trans } from 'react-i18next';
