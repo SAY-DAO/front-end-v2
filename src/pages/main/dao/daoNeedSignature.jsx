67b0fa38151604902f6189babe3c3161754f3fe9
Gbuntu <ehsan@say.company>
2023-09-02 17:08:22 +0330

    seprating reducers

--- src/pages/main/dao/daoNeedSignature.jsx
+++ src/pages/main/dao/daoNeedSignature.jsx
@@ -249,7 +249,7 @@ export default function DaoNeedSignature() {
 
   useEffect(() => {
     dispatch(fetchNeedCoefficients(needId));
-  }, [userResult]);
+  }, []);
 
   useEffect(() => {
     if (!successOneNeedData) {
