262f041a9396fea5ed1c2f22859daaeb98a6df14
Gbuntu <ehsan@say.company>
2023-09-20 20:17:57 +0330

    contribution

--- src/components/DAO/contribution/ContributionList.jsx
+++ src/components/DAO/contribution/ContributionList.jsx
@@ -5,49 +5,53 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import InboxIcon from '@mui/icons-material/Inbox';
 import { useDispatch, useSelector } from 'react-redux';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { Grid, Typography } from '@mui/material';
+import { Card, Typography } from '@mui/material';
+import WorkOutlineIcon from '@mui/icons-material/WorkOutline';
 import { fetchAvailableContribution } from '../../../redux/actions/main/daoAction';
+import ContributionModal from '../../modals/ContributionModal';
 
 export default function ContributionList() {
   const dispatch = useDispatch();
   const { t } = useTranslation();
 
+  const [open, setOpen] = useState(false);
+  const [data, setData] = useState({});
   const ecoContribution = useSelector((state) => state.ecoContribution);
   const { contributions } = ecoContribution;
 
   useEffect(() => {
     dispatch(fetchAvailableContribution());
   }, []);
-
+  const handlePopUp = (contribution) => {
+    setOpen(true);
+    setData({ ...contribution });
+  };
   return (
-    <Box sx={{ width: '100%', maxWidth: 360, mb: 15 }}>
+    <Box sx={{ width: '100%', mb: 15 }}>
+      <Typography sx={{ textAlign: 'center', mb: 2, mt: 2, fontWeight: 400 }}>
+        {t('dao.contributionTab.title')}
+      </Typography>
       <nav aria-label="main mailbox folders">
         <List>
-          <Grid container>
-            <Typography sx={{ m: 'auto', p: 4 }}>
-              {t('dao.contributionTab.notAvailable')}
-            </Typography>
-          </Grid>
           {contributions &&
             contributions.map((c) => (
-              <ListItem
-                key={c.id}
-                sx={{ border: '0.01em solid lightGrey', borderRadius: 3, mb: 2 }}
-              >
-                <ListItemButton>
-                  <ListItemIcon>
-                    <InboxIcon />
-                  </ListItemIcon>
-                  <ListItemText primary="Drafts" />
-                </ListItemButton>
-              </ListItem>
+              <Card sx={{ border: '0.01em solid lightGrey', borderRadius: 2, mb: 2 }}>
+                <ListItem key={c.id}>
+                  <ListItemButton onClick={() => handlePopUp(c)}>
+                    <ListItemIcon>
+                      <WorkOutlineIcon />
+                    </ListItemIcon>
+                    <ListItemText primary={c.title} />
+                  </ListItemButton>
+                </ListItem>
+              </Card>
             ))}
         </List>
       </nav>
+      <ContributionModal open={open} setOpen={setOpen} data={data} />
     </Box>
   );
 }
--- src/components/DAO/tabs/DaoContribution.jsx
+++ src/components/DAO/tabs/DaoContribution.jsx
@@ -1,17 +1,12 @@
 import React from 'react';
-import { Container, Typography } from '@mui/material';
-import { useTranslation } from 'react-i18next';
+import { Container } from '@mui/material';
 import ContributionList from '../contribution/ContributionList';
 import ContributionNetwork from '../contribution/ContributionNetwork';
 
 const DaoContribution = () => {
-  const { t } = useTranslation();
   return (
     <Container>
       <ContributionNetwork />
-      <Typography sx={{ textAlign: 'center', mb: 2, mt: 2, fontWeight: 400 }}>
-        {t('dao.contributionTab.title')}
-      </Typography>
       <ContributionList />
     </Container>
   );
