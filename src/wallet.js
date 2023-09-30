987ab1238b57dfdf216184a24e92d4b534ca0991
Gbuntu <ehsan@say.company>
2023-09-30 17:07:40 +0330

    wallet 403 issue

--- src/wallet.js
+++ src/wallet.js
@@ -5,14 +5,15 @@ import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
 import { createPublicClient, http } from 'viem';
 import { alchemyProvider } from '@wagmi/core/providers/alchemy';
 import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
-import { InjectedConnector } from 'wagmi/connectors/injected'
+import { InjectedConnector } from 'wagmi/connectors/injected';
+import { WalletConnectConnector } from '@wagmi/core/connectors/walletConnect';
 
 export const { chains, publicClient, webSocketPublicClient } = configureChains(
   [mainnet, sepolia],
   [
     jsonRpcProvider({
       rpc: () => ({
-        http: "https://blue-green-borough.discover.quiknode.pro/e1f6c0a3a5bd822bcb8908b1420d80e51c679b07/" // 👈 Replace this with your HTTP URL from the previous step
+        http: `https:///mainnet.infura.io/ws/v3/${process.env.REACT_APP_INFURA_KEY}`,
       }),
     }),
     alchemyProvider({ apiKey: process.env.REACT_APP_ALCHEMY_KEY }),
@@ -22,36 +23,33 @@ export const { chains, publicClient, webSocketPublicClient } = configureChains(
 );
 
 // Set up client
-export const config = createConfig(
-  {
-    autoConnect: false,
-    connectors: [
-      new InjectedConnector({
-        chains,
-        options: {
-          name: 'Injected',
-          shimDisconnect: true,
-        },
-      }),
-      new MetaMaskConnector({ chains }),
-      // new WalletConnectConnector({
-      //   chains,
-      //   options: {
-      //     relayUrl: `wss://mainnet.infura.io/ws/v3/${process.env.REACT_APP_INFURA_KEY}`,
-      //     projectId: process.env.REACT_APP_WC_PROJECT_ID,
-      //     showQrModal: false,
-      //   },
-      // }),
-    ],
-
-    publicClient: createPublicClient({
-      // batch: {
-      //   multicall: true,
-      // },
-      transport: http(),
-      // transport: webSocket(`wss://mainnet.infura.io/ws/v3/${process.env.REACT_APP_INFURA_KEY}`),
-      chain: mainnet,
+export const config = createConfig({
+  autoConnect: false,
+  connectors: [
+    new InjectedConnector({
+      chains,
+      options: {
+        name: 'Injected',
+        shimDisconnect: true,
+      },
     }),
-    webSocketPublicClient,
-  },
-);
+    new MetaMaskConnector({ chains }),
+    new WalletConnectConnector({
+      chains,
+      options: {
+        relayUrl: `wss://blue-green-borough.discover.quiknode.pro/${process.env.REACT_APP_QUICK_NODE_KEY}`,
+        projectId: process.env.REACT_APP_WC_PROJECT_ID,
+      },
+    }),
+  ],
+
+  publicClient: createPublicClient({
+    // batch: {
+    //   multicall: true,
+    // },
+    transport: http(),
+    // transport: webSocket(`wss://mainnet.infura.io/ws/v3/${process.env.REACT_APP_INFURA_KEY}`),
+    chain: mainnet,
+  }),
+  webSocketPublicClient,
+});
