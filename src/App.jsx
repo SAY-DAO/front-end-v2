e242f6e85a944dd1d01ee2be1f250c35033d959e
Gbuntu <ehsan@say.company>
2023-10-03 11:02:36 +0330

    thirdweb provider test

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
-/* eslint-disable no-unused-vars */
 import React from 'react';
-import { useRoutes } from 'react-router-dom';
+// import { useRoutes } from 'react-router-dom';
 import CssBaseline from '@mui/material/CssBaseline';
 import Container from '@mui/material/Container';
 import rtlPlugin from 'stylis-plugin-rtl';
@@ -11,17 +10,17 @@ import rtl from 'jss-rtl';
 import { StylesProvider, jssPreset } from '@mui/styles';
 import { ThemeProvider } from '@mui/material/styles';
 import { useSelector } from 'react-redux';
-import { WagmiConfig } from 'wagmi';
-import { ConnectKitProvider, ConnectKitButton } from 'connectkit';
-import { MetaMaskUIProvider, MetaMaskButton } from '@metamask/sdk-react-ui';
-import { ThirdwebProvider, ConnectWallet, useUser } from '@thirdweb-dev/react';
-import Router from './routes/Router';
+// import { WagmiConfig } from 'wagmi';
+// import { ConnectKitProvider, ConnectKitButton } from 'connectkit';
+// import { MetaMaskUIProvider, MetaMaskButton } from '@metamask/sdk-react-ui';
+import { ThirdwebProvider, ConnectWallet } from '@thirdweb-dev/react';
+// import Router from './routes/Router';
 import ThemeSettings from './layouts/customizer/ThemeSettings';
-import { config } from './wallet';
+// import { config } from './wallet';
 import RTL from './layouts/customizer/RTL';
 
 function App() {
-  const routing = useRoutes(Router);
+  // const routing = useRoutes(Router);
   const themOptions = useSelector((state) => state.themOptions);
   // const { user } = useUser();
 
@@ -39,67 +38,53 @@ function App() {
 
   const activeChain = 'ethereum';
   return (
-    <WagmiConfig config={config}>
-      <MetaMaskUIProvider
-        sdkOptions={{
-          dappMetadata: {
-            name: 'Demo UI React App',
-          },
-        }}
-      >
-        {/* <ConnectKitProvider> */}
-          <CacheProvider value={cacheRtl}>
-            <StylesProvider jss={jss}>
-              <div id="direction" dir="">
-                <CssBaseline />
-                <Container
-                  sx={{
-                    margin: 'auto',
-                    paddingLeft: '0px !important',
-                    paddingRight: '0px !important',
+    <CacheProvider value={cacheRtl}>
+      <StylesProvider jss={jss}>
+        <div id="direction" dir="">
+          <CssBaseline />
+          <Container
+            sx={{
+              margin: 'auto',
+              paddingLeft: '0px !important',
+              paddingRight: '0px !important',
+            }}
+            maxWidth="lg"
+          >
+            <ThemeProvider theme={theTheme}>
+              <RTL direction={themOptions && themOptions.activeDir}>
+                <ThirdwebProvider
+                  clientId={process.env.REACT_APP_THIRD_WEB_ID}
+                  activeChain={activeChain}
+                  authConfig={{
+                    domain: 'example.org',
+                    authUrl: '/',
+                    loginRedirect: '/',
                   }}
-                  maxWidth="lg"
                 >
-                  <ThemeProvider theme={theTheme}>
-                    <RTL direction={themOptions && themOptions.activeDir}>
-                      <ThirdwebProvider
-                        clientId={process.env.REACT_APP_THIRD_WEB_ID}
-                        activeChain={activeChain}
-                        authConfig={{
+                  <CssBaseline />
+                  {/* hint: if on useEffect will Dispatch twice to check for errors */}
+                  {/* <React.StrictMode> */}
+                  {/* {routing} */}
+                  {/* </React.StrictMode> */}
+                  <div>
+                    <div>
+                      <ConnectWallet
+                        auth={{
                           domain: 'example.org',
-                          authUrl: "/",
+                          // authUrl: "/api/auth",
                           loginRedirect: '/',
                         }}
-                      >
-                        <CssBaseline />
-                        {/* hint: if on useEffect will Dispatch twice to check for errors */}
-                        {/* <React.StrictMode> */}
-                        {routing}
-                        {/* </React.StrictMode> */}
-                        <div>
-                          <div>
-                            <ConnectWallet
-                              auth={{
-                                domain: 'example.org',
-                                // authUrl: "/api/auth",
-                                loginRedirect: '/',
-                              }}
-                            />
-                          </div>
-                          {/* {user && <p>You are signed in as {user.address}</p>} */}
-                        </div>
-                      </ThirdwebProvider>
-                    </RTL>
-                  </ThemeProvider>
-                </Container>
-              </div>
-            </StylesProvider>
-          </CacheProvider>
-          {/* <ConnectKitButton />
-          <MetaMaskButton theme="light" color="white" />
-        </ConnectKitProvider> */}
-      </MetaMaskUIProvider>
-    </WagmiConfig>
+                      />
+                    </div>
+                    {/* {user && <p>You are signed in as {user.address}</p>} */}
+                  </div>
+                </ThirdwebProvider>
+              </RTL>
+            </ThemeProvider>
+          </Container>
+        </div>
+      </StylesProvider>
+    </CacheProvider>
   );
 }
 
