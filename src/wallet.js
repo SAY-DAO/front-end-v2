df66a8dd6f84d56b93ba6a2d79e840aed652a1d4
Gbuntu <ehsan@say.company>
2023-10-04 10:47:48 +0330

    only wagmi

--- src/wallet.js
+++ src/wallet.js
@@ -1,48 +1,38 @@
 import { createConfig, configureChains, mainnet, sepolia } from 'wagmi';
-import { publicProvider } from 'wagmi/providers/public';
-import { alchemyProvider } from '@wagmi/core/providers/alchemy';
 import { infuraProvider } from 'wagmi/providers/infura';
-import { createPublicClient, webSocket } from 'viem';
-import { jsonRpcProvider } from 'wagmi/providers/jsonRpc';
-import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
+import { publicProvider } from 'wagmi/providers/public';
 import { MetaMaskConnector } from 'wagmi/connectors/metaMask';
+import { WalletConnectConnector } from 'wagmi/connectors/walletConnect';
+import { createPublicClient,  webSocket } from 'viem';
 
-export const { chains, publicClient } = configureChains(
+// Two popular providers are Alchemy (alchemy.com) and Infura (infura.io)
+export const { chains, publicClient, webSocketPublicClient } = configureChains(
   [mainnet, sepolia],
   [
-    jsonRpcProvider({
-      rpc: () => ({
-        http: `https://eth.getblock.io/${process.env.REACT_APP_GET_BLOCK_KEY}/mainnet/`,
-      }),
-    }),
-    alchemyProvider({ apiKey: process.env.REACT_APP_ALCHEMY_KEY }),
-    infuraProvider({ apiKey: process.env.REACT_APP_INFURA_KEY, stallTimeout: 1_000 }),
+    infuraProvider({ apiKey: process.env.REACT_APP_INFURA_KEY, stallTimeout: 8000 }),
     publicProvider(),
   ],
 );
 
 // Set up client
 export const config = createConfig({
-  autoConnect: false,
+  autoConnect: true,
   connectors: [
     new MetaMaskConnector({ chains }),
-
     new WalletConnectConnector({
       chains,
       options: {
-        relayUrl: `wss://relay.walletconnect.org`,
         projectId: process.env.REACT_APP_WC_PROJECT_ID,
       },
     }),
   ],
   publicClient: createPublicClient({
-    chain: mainnet,
     // batch: {
     //   multicall: true,
     // },
-    // transport: http(),
-    transport: webSocket(`wss://eth.getblock.io/${process.env.REACT_APP_GET_BLOCK_KEY}/mainnet`, {
-      retryCount: 5,
-    }),
+    transport: webSocket(`wss://mainnet.infura.io/ws/v3/${process.env.REACT_APP_INFURA_KEY}`),
+    chain: mainnet,
   }),
+
+  webSocketPublicClient,
 });
