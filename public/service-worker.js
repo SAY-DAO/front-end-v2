5a04d6991dc2c8e3b4489fbef192572b1d64bd67
G <ehsan@say.company>
2026-06-04 13:59:01 +0330

    sw changes

--- public/service-worker.js
+++ public/service-worker.js
@@ -8,8 +8,8 @@
    ================================================================ */
 
 // service-worker.js
-const STATIC_CACHE = 'SAY-DAPP-static-v2.3.5'; // bump this version when you deploy
-const RUNTIME_CACHE = 'SAY-DAPP-runtime-v2.3.5';
+const STATIC_CACHE = 'SAY-DAPP-static-v2.3.6'; // bump this version when you deploy
+const RUNTIME_CACHE = 'SAY-DAPP-runtime-v2.3.6';
 
 const PRECACHE_URLS = [
   '/',
